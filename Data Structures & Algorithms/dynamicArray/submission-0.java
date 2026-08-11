54b1fb5d28b389eb36c125c6bfa56c550054edf4
Wannida Polchan <144980074+wannidapolchan@users.noreply.github.com>
2026-08-11 14:44:23 -0700

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -0,0 +1,54 @@
+class DynamicArray {
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.arr = new int[capacity];
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if(size == capacity){
+            resize();
+
+        }
+
+        arr[size] = n;
+        size++;
+
+    }
+
+    public int popback() {
+        size--;
+        return arr[size];
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        int[] newArr = new int[capacity];
+        for(int i =0; i < size; i++){
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+
+    public int getSize() {  
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+
+    }
+}
